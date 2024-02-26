dbbafcad87ec300bc77584e150e3f095a084e371
Marcos Dhiogo Suba <madhsuba@rd.com.br>
2024-02-26 13:45:17 -0300

    atualização de arquivos

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,8 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('clickAgendar', () => {
+    cy.get('[data-cy="agendar"]').click();
+  });
+  
--- cypress/support/step_definitions/front/home.js
+++ cypress/support/step_definitions/front/home.js
@@ -2,13 +2,19 @@
 
 const { Given, When, Then } = require('cypress-cucumber-preprocessor/steps');
 
+// Adicione isso no início do seu arquivo de definição de passos
+Cypress.on('uncaught:exception', (err, runnable) => {
+  // retornar false aqui previne o Cypress de falhar o teste
+  return false;
+});
+
 Given('que o usuário está na tela inicial do site do Fleury', () => {
   cy.visit('https://www.fleury.com.br');
+  debugger; 
 });
 
 When('o usuário clica no botão "Agendar"', () => {
-  // Substitua 'seletorDoBotão' pelo seletor real do botão Agendar.
-  cy.get('seletorDoBotão').click();
+  cy.clickAgendar();
 });
 
 Then('o usuário é redirecionado para a página de agendamento de exames', () => {
